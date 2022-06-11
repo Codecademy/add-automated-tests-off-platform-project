b87668c67290cd6b941316ad67abe2602d08024e
faisal64 <faisalislam64@gmail.com>
2022-06-11 12:37:22 +0600

    added for pull request in the yaml file to make automated test while creating pull request

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,7 @@ def test_balance(app, client):
     del app
     res = client.get('/')
     assert res.status_code == 200
-    expected = {'balance': 100}
+    expected = {'balance': 0}
     assert expected == json.loads(res.get_data(as_text=True))
 
 def test_deposit(app, client):
